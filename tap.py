e4dc6deee9ddd700047f6699cfcf705051cf0bc3
Alec Sharp <alecsharpie@gmail.com>
2022-05-18 22:45:03 +1000

    use pyglet instead of pyaudio, much faster, also remove blank space from end of drum sound

--- tap.py
+++ tap.py
@@ -1,17 +1,16 @@
+import pyglet
 import time
 import board
 import digitalio
 import adafruit_lis3dh
+# setup accelerometer
 i2c = board.I2C()
 int1 = digitalio.DigitalInOut(board.D21)  # Set this to the correct pin for the interrupt!
 lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=int1)
 lis3dh.set_tap(1, 100)
+#setup music
+sound = pyglet.resource.media('sounds/short-kick-low-jazz-single.wav', streaming=False)
+print('Drum on!')
 while True:
-    if lis3dh.tapped:
-        print("Tapped!")
-        time.sleep(0.01)
-
-#while True:
-#	time.sleep(0.5)
-#	x, y, z = lis3dh.acceleration
-#	print(x, y, z)
+	if lis3dh.tapped:
+		sound.play()

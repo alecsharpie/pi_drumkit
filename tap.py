57371ba4a29df935b3fd1363529cf9ccb8de8e0d
Alec Sharp <alecsharpie@gmail.com>
2022-05-18 23:39:56 +1000

    adjust sensitivity

--- tap.py
+++ tap.py
@@ -7,7 +7,7 @@ import adafruit_lis3dh
 i2c = board.I2C()
 int1 = digitalio.DigitalInOut(board.D21)  # Set this to the correct pin for the interrupt!
 lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=int1)
-lis3dh.set_tap(1, 100)
+lis3dh.set_tap(1, 90)
 #setup music
 sound = pyglet.resource.media('sounds/short-kick-low-jazz-single.wav', streaming=False)
 print('Drum on!')
